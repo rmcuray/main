0286220c503bfca2b1484f5317d93d0282e229c6
rmcuray <53122685+rmcuray@users.noreply.github.com>
2019-11-27 00:46:28 +0800

    Add files via upload

---
+++ project/media player/musicplayer/list.js
@@ -0,0 +1,40 @@
+
+// Setup our new audio player class and pass it the playlist.
+var player = new Player([
+  {
+    title: 'Beginning - C418',
+    file: 'Beginning - C418',
+    howl: null
+  },
+  {
+    title: 'Cat - C418',
+    file: 'Cat - C418',
+    howl: null
+  },
+    {
+    title: 'Chirp - C418',
+    file: 'Chirp - C418',
+    howl: null
+  },
+      {
+    title: 'Chris - C418',
+    file: 'Chris - C418',
+    howl: null
+  },
+      {
+    title: 'Clark - C418',
+    file: 'Clark - C418',
+    howl: null
+  },
+      {
+    title: 'Danny - C418',
+    file: 'Danny - C418',
+    howl: null
+  },
+  
+      {
+    title: '#',
+    file: '#',
+    howl: null
+  }
+]);
