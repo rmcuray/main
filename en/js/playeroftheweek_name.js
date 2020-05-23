73b43411f98365c675ae2a1c41b39608c148b814
Rey Mark Curay <53122685+rmcuray@users.noreply.github.com>
2020-05-24 00:50:57 +0800

    Update playeroftheweek_name.js

--- en/js/playeroftheweek_name.js
+++ en/js/playeroftheweek_name.js
@@ -1,2 +1,2 @@
 document.getElementById("playeroftheweek").innerHTML=
-"<p>PNY Server hereby declare GoodTimesWithScar as Player Of The Week. Recognizing that they are really awesomeness.</p>";
+"PNY Server hereby declare GoodTimesWithScar as Player Of The Week. Recognizing that they are really awesomeness.";
